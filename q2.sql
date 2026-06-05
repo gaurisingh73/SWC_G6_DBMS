d47dfbd5cc225bc923110fc104bf0df3845c3e08
Gauri Singh <149578403+gaurisingh73@users.noreply.github.com>
2026-06-05 16:05:59 +0530

    Create q2.sql

---
+++ q2.sql
@@ -0,0 +1,20 @@
+WITH cte AS (
+    SELECT
+        transaction_id,
+        merchant_id,
+        credit_card_id,
+        amount,
+        transaction_timestamp,
+        LAG(transaction_timestamp) OVER (
+            PARTITION BY merchant_id,
+                         credit_card_id,
+                         amount
+            ORDER BY transaction_timestamp
+        ) AS prev_time
+    FROM transactions
+)
+
+SELECT COUNT(*) AS payment_count
+FROM cte
+WHERE prev_time IS NOT NULL
+  AND transaction_timestamp - prev_time <= INTERVAL '10 minutes';
